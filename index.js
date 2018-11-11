5681b2ad998117207b79f6a702be997b04cac31d
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-11 22:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const returnFirstTwoDrivers = function(drivers) {
   return drivers.slice(0,2);
-}
+};
 
 const returnLastTwoDrivers = function(drivers) {
-  return drivers.slice(-2)
-}
+  return drivers.slice(-2);
+};
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 const createFareMultiplier = function(integer) {
   return function(fare) {
-    return fare * integer
-  }
-}
+    return fare * integer;
+  };
+};
 
-console.log(createFareMultiplier(2))
-const fareDoubler = createFareMultiplier(2)
+const fareDoubler = createFareMultiplier(2);
 
-const fareTripler = createFareMultiplier(3)
+const fareTripler = createFareMultiplier(3);
+
+const fetchSpecifiedDrivers = function(drivers, callback) {
+  return callback(drivers);
+};
